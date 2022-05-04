ae7294604d2237a5ddd3c2214f1ef98bd40492f9
Greg Munro <gregmunro@Websy-Greg.local>
2022-05-04 17:32:10 +0200

    get field success, selected valued true

--- public/app.js
+++ public/app.js
@@ -1,2 +1,19 @@
-/* global */
 "use strict";
+
+/* global enigma schema */
+var session = enigma.create({
+  schema: schema,
+  url: 'wss://ec2-3-92-185-52.compute-1.amazonaws.com/anon/app/dcde8122-0c49-4cea-a935-d30145015cd6'
+});
+session.open().then(function (global) {
+  console.log(global);
+  global.openDoc('dcde8122-0c49-4cea-a935-d30145015cd6').then(function (app) {
+    app.getField('Region').then(function (field) {
+      field.selectValues([{
+        qtext: 'East'
+      }]).then(function (res) {
+        console.log(res);
+      });
+    });
+  });
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-/* global */ 
+/* global enigma schema */ 
+const session = enigma.create({
+  schema,
+  url: 'wss://ec2-3-92-185-52.compute-1.amazonaws.com/anon/app/dcde8122-0c49-4cea-a935-d30145015cd6'
+})
+
+session.open().then(global => {
+  console.log(global)
+  global.openDoc('dcde8122-0c49-4cea-a935-d30145015cd6').then(app => {
+    app.getField('Region').then(field => {
+      field.selectValues([{ qtext: 'East' }]).then(res => {
+        console.log(res)
+      })
+    })
+  })
+})
